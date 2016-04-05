6505deae0f190dc5d9ce600883ff99c70cd9ca07
huchuanbin <543624600@qq.com>
2016-04-05 16:42:22 +0800

    Xml

--- app/src/main/java/com/example/huchuanbin/propertyanimationdemo/XmlActivity.java
+++ app/src/main/java/com/example/huchuanbin/propertyanimationdemo/XmlActivity.java
@@ -11,23 +11,26 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class XmlActivity extends AppCompatActivity {
-@Bind(R.id.ivDemo)
+    @Bind(R.id.ivDemo)
     ImageView ivDemo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xml);
         ButterKnife.bind(this);
     }
+
     @OnClick(R.id.scaleX)
-    public void scaleX(){
-        Animator animator= AnimatorInflater.loadAnimator(this,R.animator.scalex);
+    public void scaleX() {
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.scalex);
         animator.setTarget(ivDemo);
         animator.start();
     }
+
     @OnClick(R.id.together)
-    public void together(){
-Animator animator=AnimatorInflater.loadAnimator(this, R.animator.together);
+    public void together() {
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.together);
         ivDemo.setPivotX(0);
         ivDemo.setPivotY(0);
         //显示的调用invalidate
